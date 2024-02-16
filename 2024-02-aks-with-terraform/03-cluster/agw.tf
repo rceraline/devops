b11ba19c3628048a5adaa0227d54b2068b904308
remiceraline <r.ceraline@gmail.com>
2024-02-16 21:42:05 +0000

    Add application gateway

---
+++ 2024-02-aks-with-terraform/03-cluster/agw.tf
@@ -0,0 +1,102 @@
+data "azurerm_subnet" "agw_01" {
+  name                 = "snet-agw-01"
+  virtual_network_name = "vnet-aks-01"
+  resource_group_name  = data.azurerm_resource_group.rg_01.name
+}
+
+
+resource "azurerm_public_ip" "pip_02" {
+  name                = "pip-02"
+  resource_group_name = data.azurerm_resource_group.rg_01.name
+  location            = data.azurerm_resource_group.rg_01.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# since these variables are re-used - a locals block makes this more maintainable
+locals {
+  backend_address_pool_name              = "agw-beap"
+  frontend_port_name                     = "agw-feport"
+  frontend_ip_configuration_name         = "agw-feip"
+  frontend_ip_private_configuration_name = "agw-pv-feip"
+  http_setting_name                      = "agw-be-htst"
+  listener_name                          = "agw-httplstn"
+  request_routing_rule_name              = "agw-rqrt"
+  redirect_configuration_name            = "agw-rdrcfg"
+}
+
+resource "azurerm_application_gateway" "agw_01" {
+  name                = "agw-01"
+  resource_group_name = data.azurerm_resource_group.rg_01.name
+  location            = data.azurerm_resource_group.rg_01.location
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "gic-01"
+    subnet_id = data.azurerm_subnet.agw_01.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.pip_02.id
+  }
+
+  frontend_ip_configuration {
+    name                          = local.frontend_ip_private_configuration_name
+    subnet_id                     = data.azurerm_subnet.agw_01.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.1.0.4"
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags,
+      frontend_port,
+      backend_address_pool,
+      backend_http_settings,
+      http_listener,
+      probe,
+      redirect_configuration,
+      request_routing_rule
+    ]
+  }
+}
