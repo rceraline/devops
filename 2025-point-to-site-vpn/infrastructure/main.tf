7d3f73e20408e758b5b0f93abfecc8cda06f2ee8
remiceraline <r.ceraline@gmail.com>
2025-01-21 15:12:04 -0500

    Add storage account + private dns resolver + vpn config

--- 2025-point-to-site-vpn/infrastructure/main.tf
+++ 2025-point-to-site-vpn/infrastructure/main.tf
@@ -3,6 +3,7 @@ resource "azurerm_resource_group" "vpn" {
   location = "Canada Central"
 }
 
+## VNET & Subnets
 resource "azurerm_virtual_network" "hub" {
   name                = "vnet-hub-01"
   location            = azurerm_resource_group.vpn.location
@@ -17,6 +18,29 @@ resource "azurerm_subnet" "gateway" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_subnet" "vpn_01" {
+  name                 = "sub-vpn-01"
+  resource_group_name  = azurerm_resource_group.vpn.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "inbound_dns" {
+  name                 = "sub-inbounddns"
+  resource_group_name  = azurerm_resource_group.vpn.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.0.3.0/24"]
+
+  delegation {
+    name = "Microsoft.Network.dnsResolvers"
+    service_delegation {
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+      name    = "Microsoft.Network/dnsResolvers"
+    }
+  }
+}
+
+## VPN
 resource "azurerm_public_ip" "vpn_01" {
   name                = "pip-vpn-01"
   location            = azurerm_resource_group.vpn.location
@@ -45,7 +69,7 @@ resource "azurerm_virtual_network_gateway" "vpn_01" {
 
   vpn_client_configuration {
     address_space        = ["10.100.0.0/24"]
-    vpn_client_protocols = ["SSTP", "IkeV2"]
+    vpn_client_protocols = ["OpenVPN"]
 
     root_certificate {
       name = "VpnRoot"
@@ -69,5 +93,67 @@ x4EkA6x5CAmBNbzSM4Xcptijkc125DNPzD8tn1PGgqu4xknNCW8XNbDhyi9WGsV8
 t2XTIPZtX8fjUMlxxt72/FWRdm7ErQnz186/P+5zwAcIqvQjBSDfyzs=
 EOF
     }
+  }
+}
+
+
+## Storage Account
+resource "azurerm_storage_account" "vpn" {
+  name                          = "stvpn20250120"
+  resource_group_name           = azurerm_resource_group.vpn.name
+  location                      = azurerm_resource_group.vpn.location
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
+  public_network_access_enabled = false
+}
+
+resource "azurerm_private_endpoint" "storage_vpn" {
+  name                = "pe-stvpn20250120"
+  location            = azurerm_resource_group.vpn.location
+  resource_group_name = azurerm_resource_group.vpn.name
+  subnet_id           = azurerm_subnet.vpn_01.id
+
+  private_service_connection {
+    name                           = "psc-stvpn20250120"
+    private_connection_resource_id = azurerm_storage_account.vpn.id
+    is_manual_connection           = false
+    subresource_names              = ["Blob"]
+  }
+
+  private_dns_zone_group {
+    name                 = "pdzg-stvpn20250120"
+    private_dns_zone_ids = [azurerm_private_dns_zone.blob.id]
+  }
+}
+
+## Private DNS Zone
+resource "azurerm_private_dns_zone" "blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.vpn.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "hub" {
+  name                  = "link-hub"
+  resource_group_name   = azurerm_resource_group.vpn.name
+  private_dns_zone_name = azurerm_private_dns_zone.blob.name
+  virtual_network_id    = azurerm_virtual_network.hub.id
+}
+
+##Dns Private Resolver
+resource "azurerm_private_dns_resolver" "vpn" {
+  name                = "dnspr-vpn-01"
+  resource_group_name = azurerm_resource_group.vpn.name
+  location            = azurerm_resource_group.vpn.location
+  virtual_network_id  = azurerm_virtual_network.hub.id
+}
+
+resource "azurerm_private_dns_resolver_inbound_endpoint" "inbound" {
+  name                    = "in-vpn-01"
+  private_dns_resolver_id = azurerm_private_dns_resolver.vpn.id
+  location                = azurerm_private_dns_resolver.vpn.location
+
+  ip_configurations {
+    private_ip_allocation_method = "Dynamic"
+    subnet_id                    = azurerm_subnet.inbound_dns.id
   }
 }
